7da26e40d88100979fc12107f72290f4d701b07a
Olivia McKee <olivia.mckee97@gmail.com>
2022-07-25 22:20:57 -0500

    fixed api routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Comment, Post } = require('../../models');
 
-// GET /api/users
+// GET /api/users 
 router.get('/', (req, res) => {
     User.findAll({
         attributes: { exclude: ['password'] }
@@ -13,7 +13,7 @@ router.get('/', (req, res) => {
         });
 });
 
-// GET /api/users/id
+// GET /api/users/id 
 router.get('/:id', (req, res) => {
     User.findOne({
         attributes: { exclude: ['password'] },
@@ -22,24 +22,25 @@ router.get('/:id', (req, res) => {
         },
         include: [
             {
-              model: Post,
-              attributes: ['id', 'title', 'post_text', 'created_at']
+                model: Post,
+                attributes: ['id', 'title', 'post_text', 'created_at']
             },
             {
-              model: Comment,
-              attributes: ['id', 'comment_text', 'created_at'],
-              include: {
-                model: Post,
-                attributes: ['title']
-              }
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
             }
-          ]
+        ]
     })
         .then(dbUserData => {
             if (!dbUserData) {
                 res.status(404).json({ message: 'No user found with this id.' });
                 return;
             }
+            res.json(dbUserData);
         })
         .catch(err => {
             console.log(err);
