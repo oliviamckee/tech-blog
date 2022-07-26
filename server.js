c9d6fa98b5a140921344678a31f7e1c11edca7e9
Olivia McKee <olivia.mckee97@gmail.com>
2022-07-26 18:29:40 -0500

    styling

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
@@ -20,7 +21,7 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
