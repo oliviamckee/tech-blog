050ef5d4ae1394d63f695e38e6f3fc24d86205bf
Olivia McKee <olivia.mckee97@gmail.com>
2022-07-26 15:48:10 -0500

    conditional stuff

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -13,17 +13,19 @@ router.get('/', (req, res) => {
 
 // post new comment 
 router.post('/', (req, res) => {
-    Comment.create({
+    if (req.session) {
+      Comment.create({
         comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-    })
+        post_id: req.body.post_id,
+        user_id: req.session.user_id
+      })
         .then(dbCommentData => res.json(dbCommentData))
         .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
+          console.log(err);
+          res.status(400).json(err);
         });
-});
+    }
+  });
 
 // delete comment 
 router.delete('/:id', (req, res) => {
