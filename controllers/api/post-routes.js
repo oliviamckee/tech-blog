5c3d0030d7b03210daa98e3627ed6aa7a7d3695a
Olivia McKee <olivia.mckee97@gmail.com>
2022-07-27 16:08:51 -0500

    edit page and more styling

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
+const withAuth = require('../../utils/auth');
 
 // get all posts 
 router.get('/', (req, res) => {
@@ -75,11 +76,11 @@ router.get('/:id', (req, res) => {
 });
 
 // create a post 
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     Post.create({
         title: req.body.title,
         post_text: req.body.post_text,
-        user_id: req.body.user_id
+        user_id: req.session.user_id
     })
         .then(dbPostData => res.json(dbPostData))
         .catch(err => {
@@ -89,7 +90,7 @@ router.post('/', (req, res) => {
 });
 
 // update post 
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
     Post.update(
         {
             title: req.body.title,
@@ -115,7 +116,7 @@ router.put('/:id', (req, res) => {
 });
 
 // delete a post 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     Post.destroy({
         where: {
             id: req.params.id
